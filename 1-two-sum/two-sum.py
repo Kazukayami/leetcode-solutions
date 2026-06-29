8bcc19c097950980598c783fb6d4eb1b0df33736
Kazukayami <adishchay.yatri32@gmail.com>
2026-06-29 20:37:21 +0530

    Time: 3 ms (55.99%) | Memory: 13 MB (67.50%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -5,17 +5,14 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        validation=0
-        n=len(nums)
-        for i in range(0,n-1):
-            for j in range(i+1,n):
-                if nums[i]+nums[j]==target:
-                    validation =1
-                    indices=[i,j]    
-        if validation!=0:
-            return indices
-        else:
-            return -1
+        dict={}
+        for i in range(len(nums)):
+            diff=target-nums[i]
+            if diff in dict:
+                return [dict.get(diff),i]
+            dict[nums[i]]=i
+
+
             
         
 
